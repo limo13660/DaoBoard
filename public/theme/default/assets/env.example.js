34b7337ad3cf8114857638e74e0747aaa3550cf3
limo13660 <wancnmbd@outlook.com>
2024-12-19 22:33:40 +0800

    1

--- public/theme/default/assets/env.example.js
+++ public/theme/default/assets/env.example.js
@@ -1,8 +1,8 @@
 window.settings = {
   // 站点标题
-  title: 'DaoBoard',
+  title: 'V2Board',
   // 站点描述
-  description: 'DaoBoard is best',
+  description: 'V2Board is best',
   // API
   host: '',
   // 主题
