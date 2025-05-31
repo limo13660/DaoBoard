7184011db614321b747159562b6edb9e2782bc85
limo13660 <wancnmbd@outlook.com>
2025-05-31 13:30:42 +0800

    1

---
+++ config/mo.php
@@ -0,0 +1,124 @@
+<?php
+return [
+	'license' => '8oEYTea931NyH4ySgDTK0003Rk6z8+A98z5YUj574dfu+YMbf5xxPD2u/UnNnmBNl96kWHl2yIkt6OV8lvz/kciUCszfLiK96IZR1+wDnAg=',
+	'crispId' => '',	//crisp客服id，可空
+	'inviteUrl' => 'https://cloud.ydgw.us/#/register?code=',	//邀请链接前缀
+	'subUrlSource' => 1,    # 订阅链接来源：1=从面板获取，2=从当前app检测后的api做拼接，3=将打包地址中的多个api拼接后做检测
+	'trafficLogShow' => false,
+	'versionUpdateForce' => false,	//强制更新版本
+	'versionUpdateJump' => false,	//强制更新跳转
+	'nodeDelayShow' => [
+		'type' => 1,
+		'discount' => 60,
+		'colorBest' => 900,
+		'colorGood' => 1500
+	],
+	'trafficUnlimited' => [
+		'value' => 99999,
+		'text' => '无限制'
+	],
+	'shop' => [
+		'shopExcludeIds' => [],
+		'description' => []
+	],
+	'agreements' => [
+		'show' => true,
+		'title' => '个人信息保护提示',
+		'content' => '感谢您使用云上部落！我们将依据<a href="https://xn--fhqrm578kvuk.com/user-agreement.html">《服务协议》</a>来帮助您了解我们在收集、使用、存储和共享您个人信息的情况以及您享有的相关权利。<br><br>1、您可以通过查看《服务协议》来了解我们可能收集、使用的您的个人信息情况；<br><br>2、基于您的明示授权，我们可能调用您的重要设备权限。我们将在首次调用时逐项询问您是否允许使用该权限，您有权拒绝或取消授权；<br><br>3、我们会采取业界先进的安全措施保护您的信息安全；<br><br>4、您可以查询、更正、删除、撤回授权您的个人信息，我们也提供账户注销的渠道。<br><br>',
+		'serviceLink' => 'https://xn--fhqrm578kvuk.com/user-agreement.html',
+		'privacyLink' => 'https://xn--fhqrm578kvuk.com/user-agreement.html'
+	],
+	'noticeList' => [
+		[
+			'show' => false,
+			'title' => '邀请返利10%',
+			'content' => '每邀请一名朋友并成为我们的会员，您将获得邀请佣金奖励(佣金比例10%)，若朋友在云上部落消费100元，您则可获得返利20元。此返利可用于购买套餐或提现！【快来和我们一起赚钱吧！】',
+			'negative' => '',
+			'position' => '',
+			'positionLink' => ''
+		],
+		[
+			'show' => true,
+			'title' => '云上部落永久官网',
+			'content' => '由于政策原因，导致域名很容易被封，这是不可控因素。为了防止失联，请牢记并收藏云上部落永久地址发布页【 云上部落.com 】',
+			'negative' => '我已知晓',
+			'position' => '收藏官网',
+			'positionLink' => 'https://xn--fhqrm578kvuk.com'
+		],
+        [
+            "show" => true,
+            "title" => "加入官方TG频道",
+            "content" => "加入频道第一时间获取优惠信息！",
+            "negative" => "取消",
+            "position" => "加入",
+            "positionLink" => "https://t.me/+LPNWD1UyHaZmMzE1"
+        ],
+	],
+	'buyTip' => [
+		'show' => true,
+		'title' => '温馨提示',
+		'content' => '订单支付完成后，请勿点击“关闭订单”，等待订单回调即可，或退出app进程重新查看套餐！'
+	],
+	'payTip' => [
+		'show' => true,
+		'title' => '温馨提示',
+		'content' => '如已付款切勿关闭此订单，耐心等候付款订单回调即可（通常1~2分钟内）。'
+	],
+	'homeNav' => [
+		'show' => true,
+		'title' => '网站推荐',
+		'list' => [
+			[
+				'text' => '云上部落官网',
+				'icon' => 'https://i3.mjj.rip/2023/07/10/c3e61eb2c26557451924b69516a88a11.png',
+				'link' => 'https://云上部落.com'
+			],
+			[
+				'text' => 'Google',
+				'icon' => 'https://i3.mjj.rip/2023/07/10/46daf515c691dffc8be5389efa01b215.webp',
+				'link' => 'https://www.google.com'
+			],
+			[
+				'text' => 'Telegram',
+				'icon' => 'https://simg.doyo.cn/imgfile/bgame/202303/08094239yadd.jpg',
+				'link' => 'https://t.me/moetors'
+			],
+			[
+				'text' => 'ChatGPT',
+				'icon' => 'https://cdnjson.com/images/2023/07/11/ChatGPT_logo.svg.png',
+				'link' => 'https://openai.com'
+			],
+			[
+				'text' => 'Facebook',
+				'icon' => 'https://simg.doyo.cn/imgfile/bgame/202303/07161609vgut.jpg',
+				'link' => 'https://www.facebook.com'
+			],
+			[
+				'text' => 'Instagram',
+				'icon' => 'https://simg.doyo.cn/imgfile/bgame/202303/07154226kh8v.jpg',
+				'link' => 'https://www.instagram.com'
+			],
+			[
+				'text' => 'Spotify',
+				'icon' => 'https://i3.mjj.rip/2023/07/10/c0e2fa09778c0a0864966f4ad16f5f7d.webp',
+				'link' => 'https://www.spotify.com'
+			],
+			[
+				'text' => 'YouTube',
+				'icon' => 'https://simg.doyo.cn/imgfile/bgame/202303/04165047scdv.jpg',
+				'link' => 'https://www.youtube.com'
+			],
+			[
+				'text' => 'Netflix',
+				'icon' => 'https://cdnjson.com/images/2023/07/11/e07a41e8afc91b3ff66ddd02e6b8378e786034721acfa948e43de85449c7971b_200.png',
+				'link' => 'https://www.netflix.com'
+			],
+			[
+				'text' => 'Disney+',
+				'icon' => 'https://cdnjson.com/images/2023/07/11/eb7202d9c9bfbc97c6f1e644dce1f58f9fbcf193ae9edff9bdda2c088cdbabf0_200.png',
+				'link' => 'https://www.disneyplus.com'
+			]
+		]
+	],
+
+];
