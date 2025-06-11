71c67604a8ae5666eb58e00a9340fb93b7063a87
limo13660 <wancnmbd@outlook.com>
2025-06-11 11:27:55 +0800

    Update dashboard.blade.php

--- public/theme/default/dashboard.blade.php
+++ public/theme/default/dashboard.blade.php
@@ -6,7 +6,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="refresh" content="5;url=https://bing.com">
     <title>每日一句</title>
     <style>
         body {
